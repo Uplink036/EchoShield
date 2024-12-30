d39cc5918f22addd4e270405574692a39bf0fc18
AdamMutzell <admt21@student.bth.se>
2024-12-30 12:15:48 +0000

    Detection with other ASR counting TP, TN, FP, FN now

--- defence/defence.py
+++ defence/defence.py
@@ -1,5 +1,6 @@
 import speech_recognition as sr
-import audio.audio as audio
+import os
+
 import Levenshtein
 import whisper
 import torch
@@ -7,14 +8,48 @@ import librosa
 import numpy as np
 import soundfile as sf
 from scipy.signal import medfilt
-from audio.whisper_functions import transcribe as transcribe_whisper
 
+
+def get_audio_data(folder_path):
+    """
+    Given a path, find all files in that path that ends with ".waw" and returns them.
+    """
+    files = os.listdir(folder_path)
+    audio_files = [folder_path + f for f in files if f.endswith(".wav")]
+    transcriptions = [f.replace(".wav", ".txt") for f in audio_files]
+    dataset = [
+        {"audio_file": f, "transcription": t} for f, t in zip(audio_files, transcriptions)
+    ]
+    return dataset
 
 class EchoShield:
     def __init__(self, whisper_model, threshold=0.8):
         self.whisper_model = whisper_model
         self.google_model = sr.Recognizer()
+        self.threshold = threshold
 
+    def _transcribe_whisper(self, input_file, cuda=False):
+        """
+        Transcribes an audio file using a model
+
+        :param model: The model to use for transcription \n
+        :param input_file: The path to the audio file \n
+        """
+
+        print("Transcribing: ", input_file)
+        try:
+            result = self.whisper_model.transcribe(
+                input_file,
+                fp16=cuda,
+                language="en"
+            )
+        except Exception as e:
+            print("Error: ", e)
+            return "Error" + str(e)
+
+        print("Transcription: ", result["text"])
+        return result["text"]
+    
     def _transcribe_google(self, input_file):
         """
         Transcribes an audio file using a model
@@ -37,7 +72,7 @@ class EchoShield:
         """
         return Levenshtein.ratio(transcription1, transcription2) >= threshold
 
-    def detect_compare(self, input_file, transcription, threshold=0.8):
+    def detect_compare(self, input_file, threshold=0.8):
         """
         Using another sr model, compare the transcription of the input file to the given transcription
         If they are roughly the same, return True, else return False
@@ -47,13 +82,15 @@ class EchoShield:
         :param threshold: The similarity threshold to consider the transcriptions the same \n
         """
 
+        transcription1 = self._transcribe_whisper(input_file)
         transcription2 = self._transcribe_google(input_file)
         similar = self._compare_transcriptions(
-            transcription, transcription2, threshold)
+            transcription1, transcription2, threshold)
         print("Similar: ", similar)
         if similar:
             return False
-        return True
+        else:
+            return True
 
     def reduce_noise(self, audio_file):
         """
@@ -77,32 +114,45 @@ class EchoShield:
 
         return reduced_audio, sr
 
+    def evaluate_defence(self, clean_folder, obfuscated_folder, detect_function):
+        """
+        Evaluates the defence function using the given folders
+
+        :param clean_folder: The folder containing clean audio files \n
+        :param obfuscated_folder: The folder containing obfuscated audio files \n
+        :param detect_function: The defence function to evaluate \n
+        """
+        clean_data = get_audio_data(clean_folder)
+        obfuscated_data = get_audio_data(obfuscated_folder)
+
+        tp, tn, fp, fn = 0, 0, 0, 0
+
+        for clean, obfuscated in zip(clean_data, obfuscated_data):
+            clean_detected = detect_function(clean["audio_file"])
+            if not clean_detected:
+                tp += 1
+            else:
+                fn += 1
+
+            obfuscated_detected = detect_function(obfuscated["audio_file"])
+            if obfuscated_detected:
+                tn += 1
+            else:
+                fp += 1
+        
+        return tp, tn, fp, fn
+
 
 if __name__ == "__main__":
     # Load the audio file
-    input_file = "data/archive/Raw JL corpus (unchecked and unannotated)/JL(wav+txt)/female2_angry_1b_2.wav"
-
-    data = audio.get_wav_info(input_file)
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     whisper_model = whisper.load_model("base").to(device)
 
-    attack = audio.make_test_attack(data, -5000, 5000)
-    print("Attack: ", attack)
-
-    obfuscated_audio = data["data"] + attack
-    audio.write_waw("obfuscated_audio.wav",
-                    data["samplerate"], obfuscated_audio)
-    transcription = transcribe_whisper(
-        whisper_model, "obfuscated_audio.wav", cuda=False if device == 'cpu' else True)
-
     echo_shield = EchoShield(whisper_model)
 
-    echo_shield.detect_compare("obfuscated_audio.wav", transcription)
-    reduced_audio, sr = echo_shield.reduce_noise("obfuscated_audio.wav")
-    print("sr: ", sr)
-    print("Reduced audio: ", reduced_audio)
-    sf.write("reduced_audio.wav", reduced_audio, sr)
-    transcription = transcribe_whisper(
-        whisper_model, "reduced_audio.wav", cuda=False if device == 'cpu' else True)
-
-    print("Transcription after reduction: ", transcription)
+    tp, tn, fp, fn = echo_shield.evaluate_defence("test_attacks/attack_empty/", "test_attacks/attack_bp/", echo_shield.detect_compare)
+    
+    print(f"True Positives: {tp}")
+    print(f"True Negatives: {tn}")
+    print(f"False Positives: {fp}")
+    print(f"False Negatives: {fn}")
